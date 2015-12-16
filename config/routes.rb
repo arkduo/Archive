86b18d43f0bf216f128e49daeb454fee5d324689
arkduo <s14g464@stmail.eng.kagawa-u.ac.jp>
2015-12-16 23:46:35 +0900

    ルートsign_upの無効化等

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, path_names: {
+    sign_up: ''
+  }
   resources :books, :except => [:new]
   resources :serials
   root :to => 'serials#index'
